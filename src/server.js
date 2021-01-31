7952a10c9c040e15e043b87a42c0a418d177eb1d
Marcos Corsi <marcosvcorsi@gmail.com>
2021-01-30 21:44:37 -0300

    chore: add nodemon and cors
    Signed-off-by: Marcos Corsi <marcosvcorsi@gmail.com>

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.listen(3000, () => console.log('Server is running'));
